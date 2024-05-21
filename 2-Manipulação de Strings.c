c8fad122b870fc4666f6ffd59a969fa16781993f
heronds <142829869+Herondsx@users.noreply.github.com>
2024-05-21 01:49:18 -0300

    Create 2-Manipulação de Strings.c

---
+++ 2-Manipulação de Strings.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int count = 0;
+
+    printf("Digite uma string: ");
+    fgets(str, sizeof(str), stdin);
+
+    for (int i = 0; str[i] != '\0'; i++) {
+        if (str[i] == 'a' || str[i] == 'e' || str[i] == 'i' || str[i] == 'o' || str[i] == 'u' ||
+            str[i] == 'A' || str[i] == 'E' || str[i] == 'I' || str[i] == 'O' || str[i] == 'U') {
+            count++;
+        }
+    }
+
+    printf("O número de vogais na string é: %d\n", count);
+
+    return 0;
+}
